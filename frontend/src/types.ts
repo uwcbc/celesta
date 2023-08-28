4907b5fe506a241c51d0a54b9db5e4955e167722
Roselyn Huynh <roselynh100@gmail.com>
2023-08-28 19:31:13 -0400

    getting backendy to worky

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,14 +1,14 @@
 export interface Member {
-  id: string
-  first_name: string
-  last_name: string
-  email: string
-  type: string
-  faculty?: string
-  studentNumber?: number
-  watIAM?: string | null
-  mainInstrument: string
-  otherInstruments?: string
-  shirt_size?: string
-  other?: string
+	id: string
+	first_name: string
+	last_name: string
+	email: string
+	member_type: number
+	faculty?: string
+	studentNumber?: number
+	watIAM?: string | null
+	mainInstrument: string
+	otherInstruments?: string
+	shirt_size?: string
+	other?: string
 }
